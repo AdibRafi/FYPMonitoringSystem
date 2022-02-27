b96b64c144c97b4189ac90ec850226a08ec6ca1d
WolfsParadise5 <triggered5222@gmail.com>
2022-02-27 13:30:06 +0800

    Added supervisor login back-end

---
+++ src/loginSupervisor.php
@@ -0,0 +1,30 @@
+<?php
+
+//Change credentials here if differ
+$user_login = 'root';
+$password_login = '';
+$db = 'fypfinal';
+
+//Connect to database
+$db = new mysqli('localhost', $user_login, $password_login, $db) or die("Unabe to connect");
+
+//Cross check with the database
+$advisor_check_query = "SELECT * FROM Advisor WHERE ADVISOR_ID = '$user_login'";
+$result_query = mysqli_query($db, $advisor_check_query);
+$advisor = mysqli_fetch_assoc($result_query);
+
+if ($advisor) {
+
+    if ($advisor['ADVISOR_ID'] === $user_login && $advisor['PASSWORD'] == $password_login) {
+        echo "Log in successful!";
+    }
+    
+    else {
+        echo "Log in failed!";
+    }
+    
+}
+
+else {
+    echo "ERROR: Login broke";
+}
