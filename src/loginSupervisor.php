adfa0999601490952a226c916e1075d6af5bc177
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-03-17 00:04:29 +0800

    Update loginSupervisor.php

--- src/loginSupervisor.php
+++ src/loginSupervisor.php
@@ -2,33 +2,35 @@
 
 //Change credentials here if differ
 //User credentials
-$user_login = _GET['username'];
-$user_password = _GET['password'];
+$user_login = $_GET['username'];
+$user_password = $_GET['password'];
 
 
 //Database credentials
-$db_login_username = 'root';
-$db_login_password = '';
-$db = 'fypfinal';
+$db_login_username = "root";
+$db_login_password = "";
+$db = "fypfinal";
 
 //Connect to database
-$db = new mysqli('localhost', $user_login, $password_login, $db) or die("Unabe to connect");
+$db = new mysqli("localhost", $db_login_username, $db_login_password, $db) or die("Unable to connect");
 
 //Cross check with the database
-$advisor_check_query = "SELECT * FROM Advisor WHERE ADVISOR_ID = '$user_login'";
-$result_query = mysqli_query($db, $advisor_check_query);
-$advisor = mysqli_fetch_assoc($result_query);
+$advisor_check_query = $db ->prepare("SELECT * FROM Advisor WHERE ADVISOR_ID = ?");
+$advisor_check_query->bind_param("s",$user_login);
+$advisor_check_query->execute();
+$result_query = $advisor_check_query->get_result();
+$advisor = $result_query->fetch_assoc();
 
 if ($advisor) {
 
-    if ($advisor['ADVISOR_ID'] === $user_login && $advisor['PASSWORD'] == $password_login) {
+    if ($advisor['ADVISOR_ID'] === $user_login && $advisor['PASSWORD'] == $user_password) {
         echo "Log in successful!";
     }
-    
+
     else {
         echo "Log in failed!";
     }
-    
+
 }
 
 else {
