9669fa6fbd33f385e0be6543db15cea9df1cca04
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-03-17 15:33:29 +0800

    supervisor registration implemented

--- src/loginSupervisor.php
+++ src/loginSupervisor.php
@@ -15,7 +15,7 @@ $db = "fypfinal";
 $db = new mysqli("localhost", $db_login_username, $db_login_password, $db) or die("Unable to connect");
 
 //Cross check with the database
-$advisor_check_query = $db ->prepare("SELECT * FROM Advisor WHERE ADVISOR_ID = ?");
+$advisor_check_query = $db ->prepare("SELECT * FROM supervisor WHERE supervisor_username = ?");
 $advisor_check_query->bind_param("s",$user_login);
 $advisor_check_query->execute();
 $result_query = $advisor_check_query->get_result();
@@ -23,7 +23,7 @@ $advisor = $result_query->fetch_assoc();
 
 if ($advisor) {
 
-    if ($advisor['ADVISOR_ID'] === $user_login && $advisor['PASSWORD'] == $user_password) {
+    if ($advisor['supervisor_username'] === $user_login && $advisor['supervisor_password'] == $user_password) {
         echo "Log in successful!";
     }
 
--- src/registerSupervisor.php
+++ src/registerSupervisor.php
@@ -1,16 +1,32 @@
 <?php
     //Get registration data
-    $supervisor_name = _GET['name'];
-    $supervisor_username = _GET['username'];
-    $supervisor_password = _GET['password'];
-    $supervisor_age = _GET['age'];
-    $supervisor_email _GET['email'];
+    $supervisor_fullname = $_GET['fullname'];
+    $supervisor_username = $_GET['username'];
+    $supervisor_password = $_GET['password'];
+    $supervisor_confirmpassword = $_GET['confirmPassword'];
+    $supervisor_age = $_GET['age'];
+    $supervisor_email = $_GET['email'];
 
     //Database credentials
     $db_login_username = 'root';
     $db_login_password = '';
     $db = 'fypfinal';
 
-    //Connect to database
-    $db = new mysqli('localhost', $db_login_username, $db_login_password, $db) or die("Unable to connect");
+    if ($supervisor_password != $supervisor_confirmpassword){
+        echo "Password does not match!";
+    }else{
+        //Connect to database
+        $db = new mysqli('localhost', $db_login_username, $db_login_password, $db) or die("Unable to connect");
+        $sql = "INSERT INTO supervisor (supervisor_username,supervisor_password,supervisor_age,supervisor_email,supervisor_fullname)values(?,?,?,?,?)";
+        $register_query = $db->prepare($sql);
+        $register_query_result = $register_query->execute([$supervisor_username,$supervisor_password,$supervisor_age,$supervisor_email,$supervisor_fullname]);
+
+        if($register_query_result){
+            echo "DONE";
+        }else{
+            echo "GAY";
+        }
+
+    }
+
 ?>
