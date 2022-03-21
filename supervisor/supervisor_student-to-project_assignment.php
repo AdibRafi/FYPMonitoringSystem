56f939c1bc5f4aeea0a7ddb062be43fb150669b9
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-03-21 16:55:43 +0800

    changed supervisor htmls to php to implement php code

--- supervisor/supervisor_student-to-project_assignment.php
+++ supervisor/supervisor_student-to-project_assignment.php
@@ -1,16 +1,25 @@
+<?php
+session_start();
+
+    require ("../src/functions.php");
+    require ("../src/database.php");
+
+    $user_data = checkLogin($con);
+
+?>
 <!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Project Proposal Management</title>
+    <title>Student-To-Project Assignment</title>
     <link rel="stylesheet" href="css/supervisor_dashboard.css">
-    <link rel="stylesheet" href="css/supervisor_project_proposal_management.css">
+    <link rel="stylesheet" href="css/supervisor_student-to-project_assignment.css">
 </head>
 <body>
 <header class="header">
     <img class="menu-icon" src="../src/icon/menu_128px.png" alt="menu icon" title="Menu">
     <div class="welcome-msg">
-        Welcome, Gaylord.
+        Welcome, <?php echo $user_data['supervisor_fullname']?>
     </div>
 </header>
 <div class="container">
@@ -21,16 +30,16 @@
                     <a href="supervisor_dashboard.php"><img class="sidebar-item" src="../src/icon/goal_progress_128px.png" alt="goal setting & progress setting icon" title="Goal Setting & Progress Setting"></a>
                 </li>
                 <li>
-                    <a href="supervisor_project_proposal_management.html"><img class="sidebar-item selected" src="../src/icon/project_proposal_management_128px.png" alt="project proposal management icon" title="Project Proposal Management"></a>
+                    <a href="supervisor_project_proposal_management.php"><img class="sidebar-item" src="../src/icon/project_proposal_management_128px.png" alt="project proposal management icon" title="Project Proposal Management"></a>
                 </li>
                 <li>
-                    <a href="supervisor_projectPlanning.html"><img class="sidebar-item" src="../src/icon/project_planning_128px.png" alt="project planning icon" title="Project Planning"></a>
+                    <a href="supervisor_projectPlanning.php"><img class="sidebar-item" src="../src/icon/project_planning_128px.png" alt="project planning icon" title="Project Planning"></a>
                 </li>
                 <li>
-                    <a href="supervisor_student-to-project_assignment.html"><img class="sidebar-item" src="../src/icon/student-to-project_assignment_128px.png" alt="student-to-project assignment icon" title="Student-To-Project Assignment"></a>
+                    <a href="#"><img class="sidebar-item" src="../src/icon/student-to-project_assignment_128px.png" alt="student-to-project assignment icon" title="Student-To-Project Assignment"></a>
                 </li>
                 <li>
-                    <a href="supervisor_meetingManagement.html"><img class="sidebar-item" src="../src/icon/meeting_management_128px.png" alt="meeting management icon" title="Meeting Management"></a>
+                    <a href="supervisor_meetingManagement.php"><img class="sidebar-item" src="../src/icon/meeting_management_128px.png" alt="meeting management icon" title="Meeting Management"></a>
                 </li>
             </ul>
         </div>
@@ -49,6 +58,5 @@
 
     </div>
 </div>
-
 </body>
 </html>
