27e34aca6b94ebe57a64797872701f6c1d59fd38
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-03-26 02:18:47 +0800

    new login design

--- supervisor/supervisor_login.php
+++ supervisor/supervisor_login.php
@@ -19,62 +19,35 @@
 
 <body>
 
-<form  action="../src/loginSupervisor.php" method="get" id="mainForm">
-
-        <h1 class="welcome-msg">Login as Supervisor</h1>
-        <div class="login-box">
-
-            <table>
-                <tr>
-                    <th> </th>
-                    <th> </th>
-                </tr>
-                <tr>
-                    <td>
-                        <img src="../src/icon/person_128px.png" alt="username icon">
-                    </td>
-                    <td>
-                        <label>
-                            <?php 
-                            
-                                if(isset($_GET['username'])){ 
-                                    $username = $_GET['username'];
-                                    echo ("<input type='text' name='username' placeholder='Username' value='$username' class='required'/>");
-                                }else{
-                                    echo ("<input type='text' name='username' placeholder='Username' value='' class='required'/>");
-                                } 
-                                
-                            ?>        
-                        </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <img src="../src/icon/lock_128px.png" alt="password icon">
-                    </td>
-                    <td>
-                        <label>
-                            <input type="password" name="password" placeholder="Password" value=""
-                            class="required"/>
-                        </label>
-                    </td>
-                </tr>
-                <tr>
-                    <td>
-                        <img src="../src/icon/login_128px.png" alt="login icon">
-                    </td>
-                    <td>
-                        <input type="hidden" value="<?=$_SESSION['token']?>" name="token">
-                        <input type="submit" value="Login">
-                    </td>
-                </tr>
-                <tr>
-                    <td colspan="2">
-                        <p>Don't have any account? <a href="supervisor_register.php"> Sign Up</a></p>
-                    </td>
-                </tr>
-            </table>
-        </div>
-</form>
+    <div class="login-box">
+        <form  action="../src/loginSupervisor.php" method="get" id="mainForm">
+            <h1 class="welcome-msg">Login as Supervisor</h1>
+            <div class="login-row">
+                <img src="../src/icon/person_128px.png" alt="username icon">
+                <?php 
+                
+                    if(isset($_GET['username'])){ 
+                        $username = $_GET['username'];
+                        echo ("<input type='text' name='username' placeholder='Username' value='$username' class='required'/>");
+                    }else{
+                        echo ("<input type='text' name='username' placeholder='Username' value='' class='required'/>");
+                    } 
+                    
+                ?>        
+            </div>
+            <div class="login-row">
+                <img src="../src/icon/lock_128px.png" alt="password icon">
+                <input class="passworldField"type="password" name="password" placeholder="Password" value=""
+                class="required"/>
+            </div>
+            <div class="login-row">
+                <img src="../src/icon/login_128px.png" alt="login icon">
+                <input type="submit" value="Login">
+                <input type="hidden" value="<?=$_SESSION['token']?>" name="token">
+            </div>
+            <p>Don't have any account? <a href="supervisor_register.php"> Sign Up</a></p>
+        </form>
+    </div>
+    
 </body>
 </html>
