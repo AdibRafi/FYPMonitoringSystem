809541e3abf36c43d6d52b977c60e436301b221b
Abdjb <63862086+Abdjb@users.noreply.github.com>
2022-03-26 16:15:34 +0800

    add query to UPDATE and DELETE user from Student table

--- admin/update_user.php
+++ admin/update_user.php
@@ -1,21 +1,54 @@
 <?php
-//    session_start();
-
-//require("../src/database.php");
-
-//$sql = "SELECT * from Student";
-
-
-//if (isset($_POST['student'])) echo('<p>' . $_POST['student'] . '</p>');
+session_start();
+require('../src/database.php');
 
 $student = $_POST['student'];
+$select = $_POST['submitResult'];
+
 if (empty($student)) {
-    echo"You didn't select any name";
+    echo "<script>
+        alert('Please Select a row');
+        </script>";
+
+}else if (empty($select)){
+    echo "<script>
+        alert('Please Select Verify or Remove');
+        </script>";
 }
-else{
-    $N = count($student);
-    echo"<p>You selected $N students</p>";
-    for ($i = 0; $i < $N; $i++) {
-        echo($student[$i] . " ");
+else {
+    $studentCount = count($student);
+    $loop = 0;
+    if ($select == "verify") {
+        for ($i = 0; $i < $studentCount; $i++) {
+            $verifySQL = "UPDATE Student SET ISVERIFIED=1 WHERE STUDENT_ID = '" . $student[$i] . "'";
+            if ($con->query($verifySQL) === TRUE) {
+                $loop++;
+            }
+        }
+        if ($loop === $studentCount) {
+            echo "<script>
+        alert('Verify successful');
+        </script>";
+        }
+    } else {
+        for ($i = 0; $i < $studentCount; $i++) {
+            $removeSQL = "DELETE FROM Student WHERE STUDENT_ID = '" . $student[$i] . "'";
+            if ($con->query($removeSQL) === TRUE) {
+                $loop++;
+            }
+        }
+        if ($loop === $studentCount) {
+            echo "<script>
+        alert('Remove successful');
+        </script>";
+        }
     }
+
+//    echo"<p>You selected $N students</p>";
+//    for ($i = 0; $i < $N; $i++) {
+//        echo($student[$i] . " ");
+//    }
+
 }
+//todo: need to update location after done
+echo "<script>window.location.href='verify_user.php';</script>";
