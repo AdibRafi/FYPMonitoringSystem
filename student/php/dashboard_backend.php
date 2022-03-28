2876c0b228a6188b78160ea1dab84204ccd27d4c
WolfsParadise5 <triggered5222@gmail.com>
2022-03-29 04:53:46 +0800

    Added backend files for student

---
+++ student/php/dashboard_backend.php
@@ -0,0 +1,13 @@
+<?php
+    require("database.php");
+
+    //Get project data
+
+    //Get goal data
+
+    //Generate html query for project
+
+    //Generate html query for goal
+    
+    //Inject to website
+?>
