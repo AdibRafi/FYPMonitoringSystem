43cb9128ae8a2708aa40a231ac375e53e09c84ab
Abdjb <63862086+Abdjb@users.noreply.github.com>
2022-03-28 23:31:33 +0800

    Added remove function for student and supervisor

---
+++ admin/ListUser/listUserFunction/ListStudent_function.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require('../../../src/database.php');
+
+$student = $_POST['selectRadio'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (empty($student)) {
+        echo "<script>
+        alert('Please Select Student');
+        </script>";
+        echo "<script>window.location.href='../list_student.php';</script>";
+    }
+    else if (isset($_POST['editBtn'])) {
+        echo "<script>
+        alert('Edit Button Click');
+        </script>";
+        echo "<script>window.location.href='../list_student.php';</script>";
+    }
+    else if (isset($_POST['removeBtn'])) {
+        $removeSql = "DELETE FROM Student WHERE STUDENT_ID = '".$student."'";
+        if ($con->query($removeSql) === TRUE) {
+            echo "<script>
+        alert('$student has been removed from database');
+        </script>";
+            echo "<script>window.location.href='../list_student.php';</script>";
+        } else {
+            mysqli_connect_error();
+            echo "<script>window.location.href='../list_student.php';</script>";
+        }
+    }
+}
---
+++ admin/ListUser/listUserFunction/ListSupervisor_function.php
@@ -0,0 +1,32 @@
+<?php
+session_start();
+require('../../../src/database.php');
+
+$supervisor = $_POST['selectRadio'];
+
+if ($_SERVER['REQUEST_METHOD'] === 'POST') {
+    if (empty($supervisor)) {
+        echo "<script>
+        alert('Please Select Student');
+        </script>";
+        echo "<script>window.location.href='../list_supervisor.php.php';</script>";
+    }
+    else if (isset($_POST['editBtn'])) {
+        echo "<script>
+        alert('Edit Button Click');
+        </script>";
+        echo "<script>window.location.href='../list_supervisor.php';</script>";
+    }
+    else if (isset($_POST['removeBtn'])) {
+        $removeSql = "DELETE FROM Supervisor WHERE SUPERVISOR_ID = '".$supervisor."'";
+        if ($con->query($removeSql) === TRUE) {
+            echo "<script>
+        alert('$supervisor has been removed from database');
+        </script>";
+            echo "<script>window.location.href='../list_supervisor.php';</script>";
+        } else {
+            mysqli_connect_error();
+            echo "<script>window.location.href='../list_supervisor.php';</script>";
+        }
+    }
+}
