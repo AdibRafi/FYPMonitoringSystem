43fd9da69774902f49f592f21397fcfd522f28d1
Abdjb <63862086+Abdjb@users.noreply.github.com>
2022-03-28 23:31:44 +0800

    fix button css stuff

--- admin/VerifyUser/verify_student.php
+++ admin/VerifyUser/verify_student.php
@@ -27,23 +27,34 @@ require('../../src/database.php');
         <div class="middle-sidebar">
             <ul class="sidebar-item-list">
                 <li>
-                    <a href="verify_supervisor.php"><img class="sidebar-item" src="../../src/icon/verify_128px.png" alt="verify supervisor" title="Verify Supervisor"></a>
+                    <a href="verify_supervisor.php"><img class="sidebar-item" src="../../src/icon/verify_128px.png"
+                                                         alt="verify supervisor" title="Verify Supervisor"></a>
                 </li>
                 <li>
-                    <a href="verify_student.php"><img class="sidebar-item selected" src="../../src/icon/verify_128px.png" alt="verify student" title="Verify Student"></a>
+                    <a href="verify_student.php"><img class="sidebar-item selected"
+                                                      src="../../src/icon/verify_128px.png" alt="verify student"
+                                                      title="Verify Student"></a>
                 </li>
                 <li>
-                    <a href="#"><img class="sidebar-item" src="../../src/icon/project_planning_128px.png" alt="project planning icon" title="Project Planning"></a>
+                    <a href="../ListUser/list_student.php"><img class="sidebar-item" src="../../src/icon/project_planning_128px.png"
+                                     alt="project planning icon" title="Project Planning"></a>
+                </li>
+                <li>
+                    <a href="../ListUser/list_supervisor.php"><img class="sidebar-item"
+                                                       src="../../src/icon/project_planning_128px.png" alt="list supervisor"
+                                                       title="List Supervisor"></a>
                 </li>
             </ul>
         </div>
         <div class="bottom-sidebar">
             <ul class="sidebar-item-list">
                 <li>
-                    <img class="sidebar-item" src="../../src/icon/edit_profile_128px.png" alt="edit profile icon" title="Edit Profile">
+                    <img class="sidebar-item" src="../../src/icon/edit_profile_128px.png" alt="edit profile icon"
+                         title="Edit Profile">
                 </li>
                 <li>
-                    <a href="../../src/logout.php"><img class="sidebar-item" src="../../src/icon/logout_128px.png" alt="logout icon" title="Logout"></a>
+                    <a href="../../src/logout.php"><img class="sidebar-item" src="../../src/icon/logout_128px.png"
+                                                        alt="logout icon" title="Logout"></a>
                 </li>
             </ul>
         </div>
@@ -67,7 +78,7 @@ require('../../src/database.php');
                     if ($result->num_rows > 0) {
                         while ($row = $result->fetch_assoc()) {
                             echo '<tr onclick="selectRow(this)">
-                        <td><input type="checkbox" name="student[]" value="'.$row["STUDENT_ID"].'"></td>
+                        <td><input type="checkbox" name="student[]" value="' . $row["STUDENT_ID"] . '"></td>
                         <td>' . $row["STUDENT_ID"] . '</td>
                         <td>' . $row["NAME"] . '</td>
                         <td>' . $row["AGE"] . '</td>
@@ -95,9 +106,9 @@ require('../../src/database.php');
                             </div>
                         </div>
                     </td>
-                    <td>
-                        <input type="submit" name="submit" value="submit" class="verify-box">
-                    </td>
+                    <div class="floated">
+                        <input type="submit" name="submit" value="submit" class="submitBtn">
+                    </div>
                 </tr>
                 </tfoot>
             </form>
--- admin/VerifyUser/verify_supervisor.php
+++ admin/VerifyUser/verify_supervisor.php
@@ -11,7 +11,6 @@ require('../../src/database.php');
     <meta charset="UTF-8">
     <title>Verify User</title>
     <link rel="stylesheet" href="verify_user.css" type="text/css">
-    <script type="text/javascript" src="js/sidebar.js"></script>
     <link rel="stylesheet" href="../../supervisor/css/sidebar_header.css" type="text/css">
     <script type="text/javascript" src="verify_user.js"></script>
 
@@ -28,23 +27,34 @@ require('../../src/database.php');
         <div class="middle-sidebar">
             <ul class="sidebar-item-list">
                 <li>
-                    <a href="verify_supervisor.php"><img class="sidebar-item selected" src="../../src/icon/verify_128px.png" alt="verify supervisor" title="Verify Supervisor"></a>
+                    <a href="verify_supervisor.php"><img class="sidebar-item selected"
+                                                         src="../../src/icon/verify_128px.png" alt="verify supervisor"
+                                                         title="Verify Supervisor"></a>
                 </li>
                 <li>
-                    <a href="verify_student.php"><img class="sidebar-item" src="../../src/icon/verify_128px.png" alt="verify student" title="Verify Student"></a>
+                    <a href="verify_student.php"><img class="sidebar-item" src="../../src/icon/verify_128px.png"
+                                                      alt="verify student" title="Verify Student"></a>
                 </li>
                 <li>
-                    <a href="#"><img class="sidebar-item" src="../../src/icon/project_planning_128px.png" alt="project planning icon" title="Project Planning"></a>
+                    <a href="../ListUser/list_student.php"><img class="sidebar-item" src="../../src/icon/project_planning_128px.png"
+                                     alt="project planning icon" title="Project Planning"></a>
+                </li>
+                <li>
+                    <a href="../ListUser/list_supervisor.php"><img class="sidebar-item"
+                                                       src="../../src/icon/project_planning_128px.png" alt="list supervisor"
+                                                       title="List Supervisor"></a>
                 </li>
             </ul>
         </div>
         <div class="bottom-sidebar">
             <ul class="sidebar-item-list">
                 <li>
-                    <img class="sidebar-item" src="../../src/icon/edit_profile_128px.png" alt="edit profile icon" title="Edit Profile">
+                    <img class="sidebar-item" src="../../src/icon/edit_profile_128px.png" alt="edit profile icon"
+                         title="Edit Profile">
                 </li>
                 <li>
-                    <a href="../../src/logout.php"><img class="sidebar-item" src="../../src/icon/logout_128px.png" alt="logout icon" title="Logout"></a>
+                    <a href="../../src/logout.php"><img class="sidebar-item" src="../../src/icon/logout_128px.png"
+                                                        alt="logout icon" title="Logout"></a>
                 </li>
             </ul>
         </div>
@@ -98,9 +108,9 @@ require('../../src/database.php');
                             </div>
                         </div>
                     </td>
-                    <td>
-                        <input type="submit" name="submit" value="submit" class="verify-box">
-                    </td>
+                    <div class="floated">
+                        <input type="submit" name="submit" value="submit" class="submitBtn">
+                    </div>
                 </tr>
                 </tfoot>
             </form>
