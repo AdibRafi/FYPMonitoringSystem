b3e32abf4f1d886367fbae03383074a3b42adeed
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-03-30 03:01:26 +0800

    sidebar n header redesigned, supervisor edit profile front end / all user edit profile back end implemented

--- supervisor/js/editProfile.js
+++ supervisor/js/editProfile.js
@@ -2,7 +2,7 @@
 function changePassword(password) {
     let newPassword = prompt("Please enter your new password.","");
 
-    if (confirm("Do you wish to change your password to "+newPassword+"? Press \"OK\" to confirm, press cancel to revoke action")) {
+    if (confirm("Do you wish to change your password to "+newPassword+" ? Press \"OK\" to confirm, press cancel to revoke action")) {
         let inputPassword = prompt("Please enter your old password to proceed.","");
         if (inputPassword === password.toString()){
             window.location.href="../src/editprofile.php?changePasword="+newPassword;
@@ -14,9 +14,9 @@ function changePassword(password) {
 }
 
 function changeEmail(password) {
-    let newEmail = prompt("Please enter your new password.","");
+    let newEmail = prompt("Please enter your new email.","");
     
-    if (confirm("Do you wish to change your email to "+newEmail+"? Press \"OK\" to confirm, press cancel to revoke action")) {
+    if (confirm("Do you wish to change your email to "+newEmail+" ? Press \"OK\" to confirm, press cancel to revoke action")) {
         let inputPassword = prompt("Please enter your password to proceed.","");
         if (inputPassword === password.toString()){
             window.location.href="../src/editprofile.php?changeEmail="+newEmail;
