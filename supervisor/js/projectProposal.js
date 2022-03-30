ae7792f0e7fd5d44c59cc8503bb0b20e9927799a
xinnan01991 <98335127+Xinnan-Alex@users.noreply.github.com>
2022-03-30 21:42:36 +0800

    supervispr project proposal front end

---
+++ supervisor/js/projectProposal.js
@@ -0,0 +1,17 @@
+var proposeProjectBtn = document.getElementsByClassName("propose-project-btn").item(0);
+
+var proposeProjectBox = document.getElementsByClassName("project-propose-box").item(0);
+
+var projectListBox = document.getElementsByClassName("proposed-project-list").item(0);
+
+var closeBtn = document.getElementsByClassName("close-btn").item(0);
+
+proposeProjectBtn.addEventListener("click",()=>{
+    proposeProjectBox.classList.toggle("visible");
+    projectListBox.classList.toggle("hidden");
+})
+
+closeBtn.addEventListener("click",()=>{
+    proposeProjectBox.classList.toggle("visible");
+    projectListBox.classList.toggle("hidden");
+})
