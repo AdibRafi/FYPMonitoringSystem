219292731f66ca317285c3908b7444e4c09229a8
Abdjb <63862086+Abdjb@users.noreply.github.com>
2022-04-12 14:25:24 +0800

    add function to make non approval project

--- supervisor/js/approveProject.js
+++ supervisor/js/approveProject.js
@@ -14,6 +14,7 @@ const title = document
 
 const closeBtn = document.getElementsByClassName("close-btn").item(0);
 const approveBtn = document.getElementsByClassName("approve-btn").item(0);
+const removeBtn = document.getElementsByClassName("remove-btn").item(0);
 
 var projectID;
 
@@ -36,3 +37,10 @@ approveBtn.addEventListener("click", () => {
       "../src/approveProjectProposal.php?projectID=" + projectID;
   }
 });
+
+removeBtn.addEventListener("click",()=>{
+  if (confirm("Are you sure you want to remove this project?")) {
+    window.location.href =
+        "../src/removeProjectProposal.php?projectID=" + projectID;
+  }
+})
